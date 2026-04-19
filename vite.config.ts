8e3edf9b39c012787fae0fdc0d00f070c9f243ae
Anshsing <13anshsingh@gmail.com>
2026-04-19 14:15:27 +0530

    fixed routes

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,13 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/portfolio/",
   server: {
     host: "::",
     port: 8080,
     hmr: {
       overlay: false,
-    },base: "/portfolio/",
+    }
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
